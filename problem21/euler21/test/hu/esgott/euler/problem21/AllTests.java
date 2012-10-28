c4cebdeed5a7d6c6d80189e5831e3c9c7d0ec8cb
Zsolt Szilagyi <esgott@gmail.com>
2012-10-28 16:04:17 +0100

    Created test suite for test cases

---
+++ problem21/euler21/test/hu/esgott/euler/problem21/AllTests.java
@@ -0,0 +1,11 @@
+package hu.esgott.euler.problem21;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ DivisorCounterTest.class, DivisorsTest.class })
+public class AllTests {
+
+}
