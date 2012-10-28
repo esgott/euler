0dbb97b69df15a0a58c07ca7963740c63c068286
Zsolt Szilagyi <esgott@gmail.com>
2012-10-28 11:54:55 +0100

    Added JUnit test for Divisors class

---
+++ problem21/euler21/test/hu/esgott/euler/problem21/DivisorsTest.java
@@ -0,0 +1,14 @@
+package hu.esgott.euler.problem21;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class DivisorsTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
