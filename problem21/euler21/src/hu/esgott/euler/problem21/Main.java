3297d58742fa04fe817b55b5ee610b9b19d8994d
Zsolt Szilagyi <esgott@gmail.com>
2012-10-28 16:30:05 +0100

    created the logic

--- problem21/euler21/src/hu/esgott/euler/problem21/Main.java
+++ problem21/euler21/src/hu/esgott/euler/problem21/Main.java
@@ -1,8 +1,33 @@
 package hu.esgott.euler.problem21;
 
+import java.util.Map;
+
 public class Main {
 
 	public static void main(String[] args) {
+		DivisorCounter counter = new DivisorCounter(new DivisorsFactory());
+
+		try {
+			counter.calculate();
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+
+		Map<Integer, Integer> store = counter.getStore();
+		int sum = 0;
+		// for "a" and "b", see euler problem 21
+		for (int a = 1; a <= 10000; a++) {
+			int b = store.get(a);
+			Integer db = store.get(b);
+			if (db != null) {
+				if (a != b && db.equals(a)) {
+					sum += a;
+					System.out.println(a);
+				}
+			}
+		}
+
+		System.out.println("Sum=" + sum);
 	}
 
 }
