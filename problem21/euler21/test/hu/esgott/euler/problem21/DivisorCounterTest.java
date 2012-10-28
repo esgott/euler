97de95b2706f17717626358728221cf2b9e581d3
Zsolt Szilagyi <esgott@gmail.com>
2012-10-28 16:29:15 +0100

    import organization in DivisorCounterTest

--- problem21/euler21/test/hu/esgott/euler/problem21/DivisorCounterTest.java
+++ problem21/euler21/test/hu/esgott/euler/problem21/DivisorCounterTest.java
@@ -1,9 +1,7 @@
 package hu.esgott.euler.problem21;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.times;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
 import static org.mockito.Mockito.when;
 
 import java.util.Map;
