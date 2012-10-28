25c12be8354a4bdae301e78c2494556ba34408c5
Zsolt Szilagyi <esgott@gmail.com>
2012-10-28 12:14:35 +0100

    added the ability to store the sums of an interval of numbers

--- problem21/euler21/src/hu/esgott/euler/problem21/Divisors.java
+++ problem21/euler21/src/hu/esgott/euler/problem21/Divisors.java
@@ -1,6 +1,18 @@
 package hu.esgott.euler.problem21;
 
+import java.util.Map;
+
 public class Divisors implements Runnable {
+
+	private int from;
+	private int until;
+	private Map<Integer, Integer> numberToSum;
+
+	public Divisors(int startNumber, int endNumber, Map<Integer, Integer> store) {
+		from = startNumber;
+		until = endNumber;
+		numberToSum = store;
+	}
 
 	public static int getSumOfDivisors(int number) {
 		int sum = 0;
@@ -14,6 +26,10 @@ public class Divisors implements Runnable {
 
 	@Override
 	public void run() {
+		for (int i = from; i <= until; i++) {
+			int sum = getSumOfDivisors(i);
+			numberToSum.put(i, sum);
+		}
 	}
 
 }
